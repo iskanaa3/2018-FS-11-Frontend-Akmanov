ef4e3daa69e6065230f39f567aca402b63906dee
iskach <iskanaa3@rambler.ru>
2018-10-13 19:53:45 +0300

    0

--- src/lib/test.js
+++
@@ -1,8 +0,0 @@
-const say = function (name) {
-    let div = document.createElement('div');
-	div.setAttribute('style','background-color:black; color:white;');
-	div.innerHTML = `<p>Hello ${name}</p>`;
-	document.body.appendChild(div);
-};
-
-export default say;
